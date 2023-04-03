6f236875b57d6469f27bf87af8dbc96f0e073e1b
santo0912 <santi.mendez.smz@gmail.com>
2023-04-03 17:25:55 +0000

    nuevo simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+// //create your first component
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div>icono</div>
+			<div>{props.hora2}</div>
+			<div>{props.hora1}</div>
+			<div>{props.min2}</div>
+			<div>{props.min1}</div>
+			<div>{props.seg2}</div>
+			<div>{props.seg1}</div>
 		</div>
 	);
 };
+
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,48 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let seg1 = 0
+let seg2 = 0
+let min1 = 0   
+let min2 = 0
+let hora1 = 0
+let hora2 = 0
+
+setInterval(SecondsCounter,1000);
+
+function SecondsCounter() {
+    seg1++;
+    if (seg1 > 9) {
+        seg2++;
+        seg1 = 0;
+    }
+    if (seg2 > 5) {
+        min1++;
+        seg2 = 0;
+    }
+    if (min1 > 9) {
+        min2++;
+        min1 = 0;
+    }
+    if (min2 > 5) {
+        hora1++;
+        min2 = 0;
+    }
+    if (hora1 > 9) {
+        hora2++;
+        hora1 = 0;
+    }
+    if (hora2 > 9) {
+        hora2++;
+    }
+    console.log(hora2,hora1,min2, min1,seg2,seg1);
+    
+    ReactDOM.render(<Home seg1={seg1} seg2={seg2} min1={min1} min2={min2} hora1={hora1} hora2={hora2} />, document.querySelector("#app"));
+ }
+
+// setCounter (() => {
+
+// }) 
+
+ 
